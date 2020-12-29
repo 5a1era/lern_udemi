b1e48b188115f4bc8a1bbe041b3e7fc0f315db6b
Valeriy Malyshov <valera.malyshov@gmail.com>
2020-12-29 23:53:17 +0300

    Created object personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-let numberOfFilms;
-numberOfFilms = prompt("How many movies have you watched?", "Enter number");
+const numberOfFilms = +prompt("How many movies have you watched?", "Enter number"); //Answer user
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt("One of the last watched films?", ""),
+      b = prompt("How much would you rate it?", ""),
+      c = prompt("One of the last watched films", ""),
+      d = prompt("How much would you rate it?", "");
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
