be1a7b931ec700fa0d7b51adc2f300c18bb12507
Valeriy Malyshov <valera.malyshov@gmail.com>
2021-01-10 23:40:38 +0300

    Add logo and navigation

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 "use strict";
 
-const numberOfFilms = +prompt("How many movies have you watched?", "Enter number"); //Answer user
+// const numberOfFilms = +prompt("How many movies have you watched?", "Enter number"); //Answer user
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// };
 
-const a = prompt("One of the last watched films?", ""),
-      b = prompt("How much would you rate it?", ""),
-      c = prompt("One of the last watched films", ""),
-      d = prompt("How much would you rate it?", "");
+// const a = prompt("One of the last watched films?", ""),
+//       b = prompt("How much would you rate it?", ""),
+//       c = prompt("One of the last watched films", ""),
+//       d = prompt("How much would you rate it?", "");
       
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
